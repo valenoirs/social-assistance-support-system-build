a063816cba87053f2093ee3c75eb2e4b35128cf4
valenoirs <luckyvw.general@gmail.com>
2025-03-20 15:27:16 +0700

    update: new 'origin' field in 'family' model

--- models/keluarga.js
+++ models/keluarga.js
@@ -2,10 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Keluarga = void 0;
 const mongoose_1 = require("mongoose");
-const KeluargaSchema = new mongoose_1.Schema({
+const KeluargaSchema = new mongoose_1.Schema(
+  {
     name: { type: String, required: true, unique: true },
+    origin: { type: String, required: false },
     criteria: { type: [Number], default: [1, 1, 1, 1, 1, 1, 1] },
-}, {
+  },
+  {
     timestamps: true,
-});
-exports.Keluarga = (0, mongoose_1.model)('Keluarga', KeluargaSchema);
+  }
+);
+exports.Keluarga = (0, mongoose_1.model)("Keluarga", KeluargaSchema);
--- utils/saw.js
+++ utils/saw.js
@@ -1,11 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const percent = [25, 20, 15, 15, 10, 10, 5];
-function count(arr, namaKeluarga) {
+function count(arr, namaKeluarga, desaAsal) {
     const hasil = [];
     arr.forEach((element, index) => {
-        const min = Math.max(...arr[index]);
-        hasil.push(arr[index].map((e) => e / min ));
+        const max = Math.max(...arr[index]);
+        hasil.push(arr[index].map((e) => e / max ));
     });
     const output = [];
     let real = 0;
@@ -17,7 +17,7 @@ function count(arr, namaKeluarga) {
         if (real < 0) {
             real = 0;
         }
-        output.push({ name: namaKeluarga[i], score: Math.round(real) });
+        output.push({ name: namaKeluarga[i], origin: desaAsal[i], score: Math.round(real) });
     }
     return output.sort((a, b) => {
         return b.score - a.score;
@@ -27,16 +27,18 @@ function processArray(arr) {
     const result = [];
     const criteria = [];
     const namaKeluarga = [];
-    arr.forEach((petani) => {
-        criteria.push(petani.criteria);
-        namaKeluarga.push(petani.name);
+    const desaAsal = [];
+    arr.forEach((keluarga) => {
+        criteria.push(keluarga.criteria);
+        namaKeluarga.push(keluarga.name);
+        desaAsal.push(keluarga.origin)
     });
     if (!criteria.length)
         return [];
     criteria[0].forEach((element, index) => {
         result.push(criteria.map((e, indexMap) => criteria[indexMap][index]));
     });
-    const output = count(result, namaKeluarga);
+    const output = count(result, namaKeluarga, desaAsal);
     return output;
 }
 exports.default = processArray;
